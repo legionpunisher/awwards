8c2f10711d0eab6d025f5f6f36c34ff4124a55ac
legionpunisher <mbuguamoses656@gmail.com>
2020-10-28 04:06:55 +0300

    created a home page and render redirected to our index.html file

--- review/views.py
+++ review/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+    '''
+    function that returns the index page
+    '''
+    project = Project.objects.all
+    return render(request,'index.html',{'content': project})
