6c145c7a09dcd6816cd9ebbbdd3ee75718a479a8
legionpunisher <mbuguamoses656@gmail.com>
2020-10-28 04:15:54 +0300

    created a profile view function with a login required class decorator

--- review/views.py
+++ review/views.py
@@ -8,3 +8,12 @@ def home(request):
     '''
     project = Project.objects.all
     return render(request,'index.html',{'content': project})
+
+@login_required(login_url='/accounts/login/')
+def profile(request,iden):
+    '''
+    function to return the profile of users
+    '''
+    profile = Profile.get_profile(identity=iden)
+    project = Project.get_project(identity=iden)
+    return render(request,'profile.html',{'project':project,'profile':profile})
