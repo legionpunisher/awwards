c3d42d93594cd67e97f3ddcee5b5a6a9e56f39b3
legionpunisher <mbuguamoses656@gmail.com>
2020-10-28 04:47:39 +0300

    created a rate function in our views.py

--- review/views.py
+++ review/views.py
@@ -42,3 +42,64 @@ def search(request):
 
     else:
         return render(request,'search.html')
+
+@login_required(login_url='/accounts/login/')
+def project(request,id):
+    '''
+    function to return a single project
+    '''
+    project = Project.search(name=id)
+    return render(request,'project.html',{'project': project})
+
+
+@login_required(login_url='/accounts/login/')
+def rate(request,id):
+    current_user = request.user
+    item = Project.single_project(id=id)
+    project = get_object_or_404(Project, pk= id)
+    if  request.method == 'POST':
+        form = RatingForm(request.POST, request.FILES)
+        if form.is_valid():
+            rate = form.save(commit=False)
+            rate.rater = current_user
+            rate.task = project
+            rate.average = (rate.content + rate.design + rate.usability)/3
+            rate.save()
+
+        return redirect('home')
+
+    else:
+        form = RatingForm()
+
+    return render(request,'rate.html',{'form':form,'project':item})
+
+class ProfApi(APIView):
+    def get (self, request, format=None):
+        profile = Profile.objects.all()
+        prof = Profileserializer(profile,many=True)
+
+        return Response(prof.data)
+
+class ProjApi(APIView):
+    def get(self, request, format=None):
+        project = Project.objects.all()
+        proj = Projectserializer(project, many=True)
+
+        return Response(proj.data)
+
+@login_required(login_url='/accounts/login/')
+def new_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.name = current_user
+            profile.save()
+        return redirect('home')
+
+    else:
+        form= NewProfileForm()
+
+    return render(request, 'new_profile.html', {'form':form})
+
