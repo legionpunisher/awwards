d4569f4a653ff227a10a7e824fb94a768bc2943c
legionpunisher <mbuguamoses656@gmail.com>
2020-10-28 04:58:54 +0300

    added a get functionality to our models

--- review/models.py
+++ review/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
 # Create your models here.
 class Profile(models.Model):
     name = models.ForeignKey(User,on_delete=models.CASCADE )
@@ -67,3 +70,7 @@ class Rate(models.Model):
     content = models.PositiveIntegerField(choices=Rating_choices, default=0,validators=[MaxValueValidator(10), MinValueValidator(0)])
     average = models.PositiveIntegerField(default=0,validators=[MaxValueValidator(10), MinValueValidator(0)])
     review = models.TextField(blank = True, null=True)
+
+    def __str__(self):
+        return self.rater.username
+
