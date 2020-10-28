9d551d80c4ce701d36127abc107ac0b8db9f8958
legionpunisher <mbuguamoses656@gmail.com>
2020-10-28 04:14:20 +0300

    created a url for our home and profile class module

--- review/urls.py
+++ review/urls.py
@@ -2,3 +2,9 @@ from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import  static
 from . import views
+
+urlpatterns = [
+    url('^$',views.home, name = 'home'),
+    url(r'^profile/(\w+)',views.profile,name = 'profile'),
+
+]
