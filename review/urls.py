f17b587326e927f1d2871817d162f083a96c426c
legionpunisher <mbuguamoses656@gmail.com>
2020-10-28 04:53:11 +0300

    created a url for our search, new project and project

--- review/urls.py
+++ review/urls.py
@@ -6,5 +6,8 @@ from . import views
 urlpatterns = [
     url('^$',views.home, name = 'home'),
     url(r'^profile/(\w+)',views.profile,name = 'profile'),
-
+    url(r'^new/project$',views.new_project,name = 'new_project'),
+    url(r'^search/',views.search,name='search'),
+    url(r'^project/(\w+)',views.project,name='project'),
+    url(r'^rate/(\d+)',views.rate,name='rate'),
 ]
