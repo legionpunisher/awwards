c30bfcfffb39968bf39e1ca00c3629404dcb0df2
legionpunisher <mbuguamoses656@gmail.com>
2020-10-29 06:38:17 +0300

    added requirements.txt

--- awards/settings.py
+++ awards/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'review',
+    'review',    
     'tinymce',
     'bootstrap4',
     'rest_framework',
