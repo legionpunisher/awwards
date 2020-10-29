7a699b34b35f65efd9488027752eca68f5a42e9b
legionpunisher <mbuguamoses656@gmail.com>
2020-10-29 06:14:22 +0300

    changed the timezone to Africa

--- awards/settings.py
+++ awards/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'review',
+    'tinymce',
+    'bootstrap4',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
